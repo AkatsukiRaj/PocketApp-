b9aafbc3256bc1798a57ee1ac995968b5e56c793
Rajkumar <130876120+AkatsukiRaj@users.noreply.github.com>
2026-09-12 11:47:09 +0530

    Remove app/src/main/java/com/pocket/app/widget/PocketWidgetReceiver.kt

--- app/src/main/java/com/pocket/app/widget/PocketWidgetReceiver.kt
+++
@@ -1,8 +0,0 @@
-package com.pocket.app.widget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class PocketWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = PocketGlanceWidget()
-}
